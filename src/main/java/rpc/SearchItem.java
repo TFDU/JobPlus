41fe00fa6a81ca7aa7fbd5be268fd65a62a138ce
Tianfeng Du <admiralight@gmail.com>
2020-12-17 05:16:03 -0500

    0.0.1

---
+++ src/main/java/rpc/SearchItem.java
@@ -0,0 +1,28 @@
+package rpc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+@WebServlet(name = "SearchItem", urlPatterns = {"/search"})
+public class SearchItem extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        JSONArray array = new JSONArray();
+        array.put(new JSONObject().put("username", "abcd"));
+        array.put(new JSONObject().put("username", "1234"));
+        RpcHelper.writeJsonArray(response, array);
+    }
+}
